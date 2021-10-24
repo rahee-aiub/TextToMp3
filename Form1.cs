b311942d16c8182d0c085bbf17dede7cab3067dd
Rahee <rahee.pm@gmail.com>
2021-10-24 12:51:56 +0600

    comit

--- Form1.cs
+++ Form1.cs
@@ -54,6 +54,8 @@ namespace Text2Speech
             //        sw.WriteLine("Hello World!");
             //}
 
+
+
             using (SpeechSynthesizer synth = new SpeechSynthesizer())
             {
                 synth.SetOutputToWaveFile(@"G:\Speech\SpeechTo22.mp3");
