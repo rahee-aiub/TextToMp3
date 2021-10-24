bdd1c60733a886d94d34afe55cab86389967c7b2
Rahee <rahee.pm@gmail.com>
2021-10-24 14:24:34 +0600

    comit

--- Form1.cs
+++ Form1.cs
@@ -42,25 +42,38 @@ namespace Text2Speech
                 return;
             }
 
-            //SaveFileDialog savefile = new SaveFileDialog();
-            //// set a default file name
-            //savefile.FileName = "unknown.txt";
-            //// set filters - this can be done in properties as well
-            //savefile.Filter = "Mp3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            SaveFileDialog savefile = new SaveFileDialog();
+            // set a default file name
+            savefile.FileName = "unknown.mp3";
+            // set filters - this can be done in properties as well
+            savefile.Filter = "Mp3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
 
-            //if (savefile.ShowDialog() == DialogResult.OK)
-            //{
-            //    using (StreamWriter sw = new StreamWriter(savefile.FileName))
-            //        sw.WriteLine("Hello World!");
-            //}
-
-            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            if (savefile.ShowDialog() == DialogResult.OK)
             {
-                synth.SetOutputToWaveFile(@"G:\Speech\SpeechTo22.mp3");
-                PromptBuilder builder = new PromptBuilder();
-                builder.AppendText(richTextBox1.Text);
-                synth.Speak(builder);
+                //using (StreamWriter sw = new StreamWriter(savefile.FileName))
+                  //  sw.WriteLine("Hello World!");
+
+
+                using (SpeechSynthesizer synth = new SpeechSynthesizer())
+                {
+                    synth.SetOutputToWaveFile(savefile.FileName);
+                    PromptBuilder builder = new PromptBuilder();
+                    builder.AppendText(richTextBox1.Text);
+                    synth.Speak(builder);
+
+                    MessageBox.Show("Saved Succesfully");
+                }
             }
+
+            //using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            //{
+            //    synth.SetOutputToWaveFile(@"G:\Speech\SpeechTo22.mp3");
+            //    PromptBuilder builder = new PromptBuilder();
+            //    builder.AppendText(richTextBox1.Text);
+            //    synth.Speak(builder);
+
+
+            //}
 
 
         }
