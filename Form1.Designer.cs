8c97ed4a996906ac874c15b9a5120feeb6439c0d
Rahee <rahee.pm@gmail.com>
2021-10-24 12:16:57 +0600

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Text2Speech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(592, 263);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnSpeak.FlatAppearance.BorderSize = 0;
+            this.btnSpeak.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSpeak.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpeak.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSpeak.Location = new System.Drawing.Point(146, 326);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(148, 46);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = false;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnDownload.FlatAppearance.BorderSize = 0;
+            this.btnDownload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDownload.Location = new System.Drawing.Point(358, 326);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(148, 46);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 450);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text to Speech";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text2Speech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer speechSynthesizerObj;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            speechSynthesizerObj = new SpeechSynthesizer();
+
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            speechSynthesizerObj.Dispose();
+            if (richTextBox1.Text != "")
+            {
+                speechSynthesizerObj = new SpeechSynthesizer();
+                speechSynthesizerObj.SpeakAsync(richTextBox1.Text);
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                return;
+            }
+
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                synth.SetOutputToWaveFile(@"G:\Speech\SpeechTo22.mp3");
+                PromptBuilder builder = new PromptBuilder();
+                builder.AppendText(richTextBox1.Text);
+                synth.Speak(builder);
+            }
+
+
+        }
+    }
+}
